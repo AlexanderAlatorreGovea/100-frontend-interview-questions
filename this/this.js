8701609922232b0fc2364ab0719753d55b920f0c
Alexander Govea <alexander_alatorre1993@hotmail.com>
2021-09-06 12:31:06 -0600

    resolved a file

--- this/this.js
+++ this/this.js
@@ -7,10 +7,20 @@ const exampleObj = {
     return `${this.first} ${this.last}`;
   },
   newFullName: (exampleObj) => {
-    
     return `${this.first} ${this.last}`;
   },
 };
+const key = Object.getOwnPropertyNames(exampleObj);
+
 
 console.log(exampleObj.fullName());
 console.log(exampleObj.newFullName());
+x()
+var p = 3;
+
+function x() {
+  function y() {
+    console.log(p)
+  }
+  y()
+}
